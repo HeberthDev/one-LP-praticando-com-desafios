ad07db43773d7e47f0f8d3a723cbd67b4d0603cd
HeberthDev <heberthdeveloper@gmail.com>
2024-03-20 08:35:43 -0300

    Embaralhando o array de amigos

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,6 +1,10 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let listaAmigos = document.getElementById('lista-amigos');
+
+    amigos.push(amigo.value);
 
     if (listaAmigos.textContent == '') {
         listaAmigos.textContent = amigo.value;
@@ -10,4 +14,20 @@ function adicionar() {
 
     amigo.value = '';
     amigo.focus();
+}
+
+function sortear() {
+    embaralha(amigos);
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
